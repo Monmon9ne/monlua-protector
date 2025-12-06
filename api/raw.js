9a8e349e126a8829d31d6477c3b60ea4b2f5ea0d
MonEvade <168145856+Monmon9ne@users.noreply.github.com>
2025-12-06 13:57:14 +0700

    Update error message for unauthorized access

--- api/raw.js
+++ api/raw.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const id = req.url.split("/").pop().split("?")[0];
 
   if (!ua.includes("roblox")) {
-    res.status(403).send("❌ Bạn không có quyền xem code này");
+    res.status(403).send("PROTECTED BY MON LUA PROTECTOR");
     return;
   }
 
